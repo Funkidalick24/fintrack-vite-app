d7716b30be1aff37a4c5f1b45691b4c41681e944
funkidalick24 <confidencefarai3@gmail.com>
2025-06-19 04:44:58 +0200

    fix: Update navigation links to use absolute paths for consistency across pages; modify Vite config for base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
   root: "src",
+  base: "/", // or "/your-subdirectory/" if needed
   build: {
     outDir: "../dist",
     emptyOutDir: true,
